f5c22e9c5bc4e706584220225ac382be378a9819
adnien <adnienprayata@gmail.com>
2025-11-25 16:41:09 +0700

    infotype

--- spotify.h
+++ spotify.h
@@ -1,6 +1,24 @@
 #ifndef SPOTIFY_H_INCLUDED
 #define SPOTIFY_H_INCLUDED
+#include<string>
 
+struct userInfo{
+    idUser int;
+    nameUser string;
+}
 
+struct songInfo{
+    idSong int;
+    nameSong string;
+    durasi int;
+    times_played int;
+    nameArtist string;
+    idArtist int;
+}
+
+struct artist{
+    idArtist int;
+    nameArtist string;
+}
 
 #endif // SPOTIFY_H_INCLUDED
